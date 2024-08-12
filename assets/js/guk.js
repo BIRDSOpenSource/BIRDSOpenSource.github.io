b0a1d98dc10c8b5eb923e0fbc80258152a41ddac
Husseinat Etti-Balogun <hebalogun@gmail.com>
2024-08-12 22:02:25 +0900

    filled assetsjs folder

---
+++ assets/js/guk.js
@@ -0,0 +1,11 @@
+function regform() {
+    //sendTheMail();
+    $("#downloadtrial").addClass('disabled');
+    $("#downloadtrial").text("Thanks");
+}
+function subscribeform() {
+    //sendTheMail();
+    $("#subscribebtn").addClass('disabled');
+    $("#subscribebtn").text("Thanks");
+}
+
